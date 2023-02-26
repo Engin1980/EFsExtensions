a070f14e5fe63f50f9826765ff472de909517aff
Marek Vajgl <engin@seznam.cz>
2023-02-26 09:55:01 +0100

    Merged changes from local

--- ChecklistModule/Types/Autostarts/AutostartProperty.cs
+++ ChecklistModule/Types/Autostarts/AutostartProperty.cs
@@ -3,9 +3,20 @@
   public class AutostartProperty : IAutostart
   {
     public AutostartPropertyName Name { get; set; }
+    public int NameIndex { get; set; } = 0;
     public int Value { get; set; }
     public AutostartPropertyDirection Direction { get; set; }
 
-    public string DisplayString => $"({Name} {Direction.ToString().ToLower()} {Value})";
+    public string DisplayName
+    {
+      get
+      {
+        if (NameIndex == 0)
+          return $"{Name}";
+        else
+          return $"{Name}:{NameIndex}";
+      }
+    }
+    public string DisplayString => $"({DisplayName} {Direction.ToString().ToLower()} {Value})";
   }
 }
--- ESimConnect/Types/RequestManager.cs
+++ ESimConnect/Types/RequestManager.cs
@@ -23,26 +23,36 @@ namespace ESimConnect.Types
       }
     }
 
+    private void Log(string s)
+    {
+      System.IO.File.AppendAllText("rm.txt", s + "\n");
+    }
+
     private readonly List<RData> inner = new();
 
     public void Register(int? customId, Type type, EEnum requestId)
     {
+      Log($"Registering check {customId}, {type.Name}, {requestId}");
       if (customId != null && inner.Any(q => q.customId == customId))
         throw new InvalidRequestException($"customRequestId '{customId}' is already registered.");
+      Log($"Registering {customId}, {type.Name}, {requestId}");
       inner.Add(new RData(requestId, customId, type));
     }
 
     public void Unregister(int? customId)
     {
+      Log($"UnRegistering {customId}");
       if (!inner.Any(q => q.customId == customId))
         inner.Remove(inner.Single(q => q.customId == customId));
     }
 
     public void Recall(EEnum requestId, out Type type, out int? customId)
     {
+      Log($"Recalling {requestId}");
       RData rd = inner.Single(q => q.requestId == requestId);
       type = rd.type;
       customId = rd.customId;
+      Log($"Recalled {requestId}, {type.Name}, {customId}");
     }
   }
 }
