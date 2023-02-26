e7bc6e0680e6e8978b535c9d107b440e61a5386b
Marek Vajgl <marek.vajgl@osu.cz>
2023-02-26 09:55:39 +0100

    Added missing logger file

---
+++ ESimConnect/Logger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESimConnect
+{
+  public static class Logger
+  {
+    public static Action<string>? LogHandler { get; set; } = null;
+    public static void Log(string msg, Type sender = null)
+    {
+      if (sender != null) msg = $"[{sender.Name}] {msg}";
+      LogInternal(msg);
+    }
+    private static void LogInternal(string msg)
+    {
+      LogHandler?.Invoke(msg);
+    }
+  }
+}
