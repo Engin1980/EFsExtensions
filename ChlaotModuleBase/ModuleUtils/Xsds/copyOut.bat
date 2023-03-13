63ff4abb531a8d870f47b436c8620c9a4ad394b6
Marek Vajgl <engin@seznam.cz>
2023-03-13 18:35:41 +0100

    Rewritten XSD to not use namespaces and use includes only.

--- ChlaotModuleBase/ModuleUtils/Xsds/copyOut.bat
+++ ChlaotModuleBase/ModuleUtils/Xsds/copyOut.bat
@@ -2,4 +2,4 @@ echo Copying XSD files
 cd
 copy ".\ModuleUtils\Xsds\*.xsd" ".\..\CopilotModule\Xmls\"
 copy ".\ModuleUtils\Xsds\*.xsd" ".\..\ChecklistModule\Xmls\"
-copy ".\ModuleUtils\Xsds\MetaDataSchema.xsd" ".\..\AffinityModule\Xmls\"
+copy ".\ModuleUtils\Xsds\*.xsd" ".\..\AffinityModule\Xmls\"
