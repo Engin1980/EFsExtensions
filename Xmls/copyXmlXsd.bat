ce1796c18cbb6b8f4efe4b2115a79e7d0da0b9ea
Marek Vajgl <engin@seznam.cz>
2023-09-17 22:25:18 +0200

    Updated XMLs location. Updated how XML files are copied to output.

---
+++ Xmls/copyXmlXsd.bat
@@ -0,0 +1,10 @@
+cd ..\Xmls
+cd
+
+if not exist "..\Chlaot\bin\debug\net6.0-windows\Xmls" mkdir "..\Chlaot\bin\debug\net6.0-windows\Xmls"
+copy .\Copilot\*.xml ..\Chlaot\bin\debug\net6.0-windows\Xmls
+copy .\Failures\*.xml ..\Chlaot\bin\debug\net6.0-windows\Xmls
+copy .\Checklist\*.xml ..\Chlaot\bin\debug\net6.0-windows\Xmls
+
+if not exist "..\Chlaot\bin\debug\net6.0-windows\Xmls\Xsds" mkdir "..\Chlaot\bin\debug\net6.0-windows\Xmls\Xsds"
+copy .\Xsds\*.xsd ..\Chlaot\bin\debug\net6.0-windows\Xmls\Xsds
