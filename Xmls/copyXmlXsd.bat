4841251a16e4ed1f89ce9a5da7306b71dc1b2753
Marek Vajgl <engin@seznam.cz>
2023-09-17 22:29:34 +0200

    Updated typo in xml copy bat file.

--- Xmls/copyXmlXsd.bat
+++ Xmls/copyXmlXsd.bat
@@ -5,6 +5,7 @@ if not exist "..\Chlaot\bin\debug\net6.0-windows\Xmls" mkdir "..\Chlaot\bin\debu
 copy .\Copilot\*.xml ..\Chlaot\bin\debug\net6.0-windows\Xmls
 copy .\Failures\*.xml ..\Chlaot\bin\debug\net6.0-windows\Xmls
 copy .\Checklist\*.xml ..\Chlaot\bin\debug\net6.0-windows\Xmls
+copy .\Affinity\*.xml ..\Chlaot\bin\debug\net6.0-windows\Xmls
 
 if not exist "..\Chlaot\bin\debug\net6.0-windows\Xmls\Xsds" mkdir "..\Chlaot\bin\debug\net6.0-windows\Xmls\Xsds"
 copy .\Xsds\*.xsd ..\Chlaot\bin\debug\net6.0-windows\Xmls\Xsds
