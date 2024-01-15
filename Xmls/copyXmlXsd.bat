e289a653bb592839aec18a8d5baffdf9ebd8a0db
Marek Vajgl <engin@seznam.cz>
2024-01-15 21:45:53 +0100

    Playing with xml location.

--- Xmls/copyXmlXsd.bat
+++ Xmls/copyXmlXsd.bat
@@ -2,6 +2,7 @@ cd ..\Xmls
 cd
 
 if not exist "..\Chlaot\bin\debug\net6.0-windows\Xmls" mkdir "..\Chlaot\bin\debug\net6.0-windows\Xmls"
+copy .\Global\*.xml ..\Chlaot\bin\debug\net6.0-windows\Xmls
 copy .\Copilot\*.xml ..\Chlaot\bin\debug\net6.0-windows\Xmls
 copy .\Failures\*.xml ..\Chlaot\bin\debug\net6.0-windows\Xmls
 copy .\Checklist\*.xml ..\Chlaot\bin\debug\net6.0-windows\Xmls
