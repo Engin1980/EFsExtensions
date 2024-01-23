66a998b4391861edcb123eb5ab85d3153d221d50
Marek Vajgl <engin@seznam.cz>
2024-01-23 07:57:22 +0100

    Implemented numericupdown for MTBF

---
+++ Modules/FailuresModule/Controls/NumericUpDown.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Eng.Chlaot.Modules.FailuresModule.Controls
+{
+  /// <summary>
+  /// Interaction logic for NumericUpDown.xaml
+  /// </summary>
+  public partial class NumericUpDown : UserControl
+  {
+    public NumericUpDown()
+    {
+      InitializeComponent();
+    }
+
+    public static readonly DependencyProperty ValueProperty =
+   DependencyProperty.Register(nameof(Value), typeof(double), typeof(NumericUpDown));
+
+    public double Value
+    {
+      get
+      {
+        double ret = (double)GetValue(ValueProperty);
+        return ret;
+      }
+      set
+      {
+        SetValue(ValueProperty, value);
+      }
+    }
+
+    private (int, int) GetValAndOrder(double d)
+    {
+      int order = 0;
+      int value = 0;
+
+      d = Math.Abs(d);
+      if (d >= 1)
+      {
+        while(d >= 1)
+        {
+          order++;
+          d /= 10;
+        }
+        d *= 10;
+      }
+      else
+      {
+        do
+        {
+          order--;
+          d *= 10;
+        } while (d < 1);
+      }
+      value = (int)(d % 10);
+      order--;
+      return (value, order);
+    }
+
+    public void IncreaseValue()
+    {
+      int v, o;
+      (v, o) = GetValAndOrder(this.Value);
+      double inc = (v + 1) * Math.Pow(10, o);
+      Value = inc;
+    }
+
+    public void DecreaseValue()
+    {
+      int v, o;
+      (v, o) = GetValAndOrder(this.Value);
+      double dec;
+      if (v == 1)
+        dec = 9 * Math.Pow(10, o - 1);
+      else
+        dec = (v - 1) * Math.Pow(10, o);
+      Value = dec;
+    }
+
+    private void btnUp_Click(object sender, RoutedEventArgs e)
+    {
+      IncreaseValue();
+    }
+
+    private void btnDown_Click(object sender, RoutedEventArgs e)
+    {
+      DecreaseValue();
+    }
+  }
+}
---
+++ Modules/FailuresModule/Model/Incidents/TimeTrigger.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eng.Chlaot.Modules.FailuresModule.Model.Incidents
+{
+  public class TimeTrigger : Trigger
+  {
+    private readonly static Random rnd = new();
+
+    public enum TimeTriggerInterval
+    {
+      OncePerTenSeconds,
+      OncePerMinute,
+      OncePerTenMinutes,
+      OncePerHour
+    }
+
+    private readonly int secondDigit = rnd.Next(0, 10);
+    private readonly int secondValue = rnd.Next(0, 60);
+    private readonly int minuteDigit = rnd.Next(0, 10);
+    private readonly int minuteValue = rnd.Next(0, 60);
+
+    public TimeTriggerInterval Interval
+    {
+      get => base.GetProperty<TimeTriggerInterval>(nameof(Interval))!;
+      set
+      {
+        base.UpdateProperty(nameof(Interval), value);
+        UpdateProbability();
+      }
+    }
+
+    public int MtbfHours
+    {
+      get => base.GetProperty<int>(nameof(MtbfHours))!;
+      set
+      {
+        base.UpdateProperty(nameof(MtbfHours), Math.Max(value, 1));
+        UpdateProbability();
+      }
+    }
+
+    public Func<bool> EvaluatingFunction
+    {
+      get
+      {
+        Func<bool> ret = Interval switch
+        {
+          TimeTriggerInterval.OncePerTenSeconds => () => DateTime.Now.Second % 10 == secondDigit,
+          TimeTriggerInterval.OncePerMinute => () => DateTime.Now.Second == secondValue,
+          TimeTriggerInterval.OncePerTenMinutes => () => DateTime.Now.Second == secondValue && DateTime.Now.Minute % 10 == minuteDigit,
+          TimeTriggerInterval.OncePerHour => () => DateTime.Now.Second == secondValue && DateTime.Now.Minute == minuteValue,
+          _ => throw new NotImplementedException()
+        };
+        return ret;
+      }
+      set { throw new ApplicationException($"Setting {nameof(EvaluatingFunction)} property is not possible."); }
+    }
+
+    private void UpdateProbability()
+    {
+      this.Probability = this.Interval switch
+      {
+        TimeTriggerInterval.OncePerTenSeconds => (Percentage)((double)1 / this.MtbfHours / 360),
+        TimeTriggerInterval.OncePerMinute => (Percentage)((double)1 / this.MtbfHours / 60),
+        TimeTriggerInterval.OncePerTenMinutes => (Percentage)((double)1 / this.MtbfHours / 6),
+        TimeTriggerInterval.OncePerHour => (Percentage)((double)1 / this.MtbfHours),
+        _ => throw new NotImplementedException()
+      };
+    }
+
+    public TimeTrigger()
+    {
+      this.MtbfHours = 1000;
+      this.Interval = TimeTriggerInterval.OncePerHour;
+    }
+  }
+}
