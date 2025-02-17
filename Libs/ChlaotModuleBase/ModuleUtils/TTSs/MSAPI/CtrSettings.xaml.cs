c6600d3b9cc446922bc046f811bad3c2904eff8b
Marek Vajgl <engin@seznam.cz>
2025-02-17 22:44:58 +0100

    TTS: Updated settings control for Ms-SAPI.

--- Libs/ChlaotModuleBase/ModuleUtils/TTSs/MSAPI/CtrSettings.xaml.cs
+++ Libs/ChlaotModuleBase/ModuleUtils/TTSs/MSAPI/CtrSettings.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,6 +32,12 @@ namespace Eng.Chlaot.ChlaotModuleBase.ModuleUtils.TTSs.MSAPI
     {
       InitializeComponent();
       this.DataContext = this.settings = settings;
+    }
+
+    private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+    {
+      Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+      e.Handled = true;
     }
   }
 }
