34f7c52743b26b8ec41395c06cc7c29cb4804055
Marek Vajgl <engin@seznam.cz>
2025-03-06 12:12:09 +0100

    Working on new ESimConnect.

--- Libs/EFsExtensionsModuleBase/ModuleUtils/Xsds/copyOut.bat
+++
@@ -1,5 +0,0 @@
-echo Copying XSD files
-cd
-copy ".\ModuleUtils\Xsds\*.xsd" ".\..\CopilotModule\Xmls\"
-copy ".\ModuleUtils\Xsds\*.xsd" ".\..\ChecklistModule\Xmls\"
-copy ".\ModuleUtils\Xsds\*.xsd" ".\..\AffinityModule\Xmls\"
