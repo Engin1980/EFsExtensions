74790871f6aaa24d3f84af22a918efc1ebce9fbe
Marek Vajgl <engin@seznam.cz>
2025-03-15 22:15:28 +0100

    Some additional typing issues with old 'Chlaot' naming resolved.

--- EFsExtensions/Context.cs
+++ EFsExtensions/Context.cs
@@ -22,56 +22,56 @@ namespace Eng.EFsExtensions.App
 
     internal void SetUpModules()
     {
-      logger.Invoke(LogLevel.INFO, "Loading modules...");
+      logger.Log(LogLevel.INFO, "Loading modules...");
       IModule module;
 
-      logger.Invoke(LogLevel.INFO, "Loading check-list module...");
+      logger.Log(LogLevel.INFO, "Loading check-list module...");
       module = new Eng.EFsExtensions.Modules.ChecklistModule.ChecklistModule();
       TryAddModule(module);
 
-      logger.Invoke(LogLevel.INFO, "Loading copilot module...");
+      logger.Log(LogLevel.INFO, "Loading copilot module...");
       module = new Eng.EFsExtensions.Modules.CopilotModule.CopilotModule();
       TryAddModule(module);
 
-      logger.Invoke(LogLevel.INFO, "Loading failures module...");
+      logger.Log(LogLevel.INFO, "Loading failures module...");
       module = new Eng.EFsExtensions.Modules.FailuresModule.FailuresModule();
       TryAddModule(module);
 
-      logger.Invoke(LogLevel.INFO, "Loading RaaS module...");
+      logger.Log(LogLevel.INFO, "Loading RaaS module...");
       module = new Eng.EFsExtensions.Modules.RaaSModule.RaaSModule();
       TryAddModule(module);
 
-      logger.Invoke(LogLevel.INFO, "Loading sim-var-test module...");
+      logger.Log(LogLevel.INFO, "Loading sim-var-test module...");
       module = new Eng.EFsExtensions.Modules.SimVarTestModule.SimVarTestModule();
       TryAddModule(module);
 
-      logger.Invoke(LogLevel.INFO, "Loading affinity module...");
+      logger.Log(LogLevel.INFO, "Loading affinity module...");
       module = new Eng.EFsExtensions.Modules.AffinityModule.AffinityModule();
       TryAddModule(module);
 
-      logger.Invoke(LogLevel.INFO, "Loading flight-log module...");
-      module = new Eng.Chlaot.Modules.FlightLogModule.FlightLogModule();
+      logger.Log(LogLevel.INFO, "Loading flight-log module...");
+      module = new Eng.EFsExtensions.Modules.FlightLogModule.FlightLogModule();
       TryAddModule(module);
 
-      logger.Invoke(LogLevel.INFO, "Setting up modules...");
+      logger.Log(LogLevel.INFO, "Setting up modules...");
       Modules.ToList().ForEach(q => q.SetUp(
         new ModuleSetUpInfo()));
 
-      logger.Invoke(LogLevel.INFO, "Modules loaded & set up.");
+      logger.Log(LogLevel.INFO, "Modules loaded & set up.");
     }
 
     internal void InitModules()
     {
-      logger.Invoke(LogLevel.INFO, "Initializing modules");
+      logger.Log(LogLevel.INFO, "Initializing modules");
       Modules.ToList().ForEach(q => q.Init());
-      logger.Invoke(LogLevel.INFO, "Modules initialized");
+      logger.Log(LogLevel.INFO, "Modules initialized");
     }
 
     private void TryAddModule(IModule module)
     {
       if (this.Modules.Any(q => q.Name == module.Name))
       {
-        logger.Invoke(LogLevel.ERROR, $"Unable to add '{module.Name}' module. Module with this name already exists.");
+        logger.Log(LogLevel.ERROR, $"Unable to add '{module.Name}' module. Module with this name already exists.");
       }
       else
         Modules.Add(module);
@@ -79,9 +79,9 @@ namespace Eng.EFsExtensions.App
 
     internal void RunModules()
     {
-      logger.Invoke(LogLevel.INFO, "Starting modules");
+      logger.Log(LogLevel.INFO, "Starting modules");
       Modules.ToList().ForEach(q => q.Run());
-      logger.Invoke(LogLevel.INFO, "Modules running");
+      logger.Log(LogLevel.INFO, "Modules running");
     }
 
     internal void RemoveUnreadyModules()
@@ -89,7 +89,7 @@ namespace Eng.EFsExtensions.App
       var modules = Modules.Where(q => !q.IsReady).ToList();
       foreach (var module in modules)
       {
-        logger.Invoke(LogLevel.INFO, $"Module '{module.Name}' removed as it is not-ready.");
+        logger.Log(LogLevel.INFO, $"Module '{module.Name}' removed as it is not-ready.");
         Modules.Remove(module);
       }
     }
