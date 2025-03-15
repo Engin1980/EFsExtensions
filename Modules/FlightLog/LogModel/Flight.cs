13a5ca925c7b8f0e3b827e22086c87422a5a481f
Marek Vajgl <engin@seznam.cz>
2025-03-15 09:42:15 +0100

    Added some more properties to Model

--- Modules/FlightLog/LogModel/Flight.cs
+++ Modules/FlightLog/LogModel/Flight.cs
@@ -6,9 +6,22 @@ using System.Threading.Tasks;
 
 namespace Eng.Chlaot.Modules.FlightLogModule.LogModel
 {
-  internal record StartUp(DateTime Time);
-  internal record TakeOff(DateTime Time);
-  internal record Landing(DateTime Time);
-  internal record ShutDown(DateTime Time);
-  internal record Flight(string? DepartureICAO, string? DestinationICAO, StartUp StartUp, TakeOff TakeOff, Landing Landing, ShutDown ShutDown);
+  public enum DivertReason
+  {
+    NotDiverted,
+    Weather,
+    Medical,
+    Mechanical,
+    Other
+  }
+  internal record GPS(double Latitude, double Longitude);
+  internal record StartUp(DateTime Time, double FuelAmountKg);
+  internal record TakeOff(DateTime Time, double FuelAmountKg);
+  internal record Landing(DateTime Time, double FuelAmountKg, double LandingIAS, double LandingG, double LandingPitch, double LandingGPS);
+  internal record ShutDown(DateTime Time, double FuelAmountKg);
+  internal record Flight(string? DepartureICAO, string? DestinationICAO, 
+    DateTime ScheduledOffBlockTime, DateTime ScheduledOnBlockTime, int? PassengerCount,
+    string? AircraftType, string? AircraftRegistration, string AircraftModel,
+    StartUp StartUp, TakeOff TakeOff, Landing Landing, ShutDown ShutDown, 
+    DivertReason? DivertReason);
 }
