906355c397118633f5f595ed5d0b11d729b76905
Marek Vajgl <engin@seznam.cz>
2025-04-19 00:23:12 +0200

    Added icon

--- _updateRelease.bat
+++ _updateRelease.bat
@@ -3,7 +3,7 @@ mkdir _Release
 mkdir .\_Release\EFsExtensions
 copy readme.md .\_Release\EFsExtensions\_Readme.md
 copy License .\_Release\EFsExtensions\_License.txt
-xcopy /e /i /Y \EFsExtensions\bin\debug\net6.0-windows\* .\_Release\EFsExtensions
+xcopy /e /i /Y .\EFsExtensions\bin\Debug\net6.0-windows\* .\_Release\EFsExtensions
 cd .\_Release
 tar.exe -c -f EFsExtensions.zip EFsExtensions
 
